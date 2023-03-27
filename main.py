8d021f418c36ba5782b7515f123344cd1eec6b7b
victorlisman <victor.lisman@protonmail.com>
2023-03-27 20:00:44 +0300

    Improved model and training

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+from MNISTmodel import NeuralNet
 
 device = torch.device('cpu')
 
 input_size = 784
-hidden_size = 100
+hidden_size = 800
 num_classes = 10
 num_epochs = 2
 batch_size = 100
@@ -21,28 +22,13 @@ test_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batc
 
 examples = iter(train_loader)
 samples, labels = next(examples)
-
-
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet, self).__init__()
-        self.l1 = nn.Linear(input_size, hidden_size)
-        self.l2 = nn.Linear(hidden_size, hidden_size)
-        self.l3 = nn.Linear(hidden_size, num_classes)
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        out = self.l1(x)
-        out = self.relu(out)
-        out = self.l2(out)
-        out = self.l3(out)
-        return out
     
 model = NeuralNet(input_size, hidden_size, num_classes)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+#before training
 with torch.no_grad():
     n_samples = 0
     for images, labels in test_loader:
@@ -52,9 +38,11 @@ with torch.no_grad():
 
         if n_samples < 6:
             for i in range(6):
+                plt.figure("Before Training")
                 plt.subplot(2, 3, i + 1)
                 plt.imshow(images[i].reshape(28, 28), cmap='gray')
                 plt.title(f'Prediction: {torch.max(outputs.data, 1)[1][i]}')
+
             plt.show()
 
         _, predictions = torch.max(outputs, 1)
@@ -76,6 +64,7 @@ for epoch in range(num_epochs):
         if (i+1) % 100 == 0:
             print(f'epoch {epoch+1}/{num_epochs}, step {i+1}/{n_total_steps}, loss = {loss.item():.4f}')
 
+#after training
 with torch.no_grad():
     n_correct = 0
     n_samples = 0
@@ -86,6 +75,7 @@ with torch.no_grad():
 
         if n_samples < 12:
             for i in range(6):
+                plt.figure("After Training")
                 plt.subplot(2, 3, i + 1)
                 plt.imshow(images[i].reshape(28, 28), cmap='gray')
                 plt.title(f'Prediction: {torch.max(outputs.data, 1)[1][i]}')
